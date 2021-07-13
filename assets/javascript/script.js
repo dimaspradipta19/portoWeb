453469c296ec19cf47c5dba56c8b4af78dd7c7fd
Dimas pradipta <46133782+dimaspradipta19@users.noreply.github.com>
2021-07-13 21:43:03 +0700

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,26 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
+const experience = document.querySelector('.experiences');
+const buttons = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+
+experience.addEventListener("click", function (e) {
+    const id = e.target.dataset.id;
+    if (id) {
+        //remove active from other button
+        buttons.forEach(function (button) {
+            button.classList.remove('active');
+        });
+        e.target.classList.add('active');
+
+
+        //hide other article
+        articles.forEach(function(article){
+            article.classList.remove("active");
+            console.log("Dimas")
+        });
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+});
